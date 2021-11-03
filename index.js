40a2a8efb4c060534e7a78f67c8b69bef8858fc2
Ray Lu <raylu511@gmail.com>
2021-11-03 14:42:57 +0000

    Finished challenge

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 //write your code here
+
+
+// p: declare f reverseString(string) 
+//   returns the reversed version of the string without using js reverse methods
+
+// e: "ray" -> "yar" 
+
+// d: string
+
+// algo: 
+//   //declare the variable reversedString and initialize empty string
+//   //iterate through string starting from the end
+// 	 //concat reversedString with the value of string[index] 
+
+
+function reverseString(string){
+    let reversedString = '';
+    for(let i = string.length-1 ; i>=0; i--){
+        reversedString+=string[i];
+    }
+    
+    return reversedString;
+}
+
+
+console.log(reverseString('ray'));
+console.log(reverseString("hello"))//"olleh"
+console.log(reverseString("oo"))//"oo"
+console.log(reverseString(""))//""
